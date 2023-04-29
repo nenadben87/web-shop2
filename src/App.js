371bddb8004e3da437d0b714436ddff0a8d6b864
Nenad Benke <nenad.ben87@gmail.com>
2023-04-29 13:38:52 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './routes/Home';
 import MensDep from './routes/MensDep';
